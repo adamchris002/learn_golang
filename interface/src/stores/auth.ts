a7a4f72c7faac99c457f2a2b84657a545adb2c28
Adam Christoper <adam2510025@gmail.com>
2026-07-16 15:45:50 +0700

    finalize login and register func

--- interface/src/stores/auth.ts
+++ interface/src/stores/auth.ts
@@ -1,33 +1,53 @@
-import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useAuthStore = defineStore('auth', () => {
-  const token = ref<string | null>(localStorage.getItem('token'))
-  const user = ref<{ id: string; name: string; email: string } | null>(null)
+export const useAuthStore = defineStore("auth", () => {
+  const token = ref<string | null>(localStorage.getItem("token"));
+  const user = ref<{
+    id: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+  } | null>(null);
 
-  const isAuthenticated = computed(() => !!token.value)
+  const message = ref<{ status: Number; messageTitle: string; message: string } | null>(null);
+
+  const isAuthenticated = computed(() => !!token.value);
 
   function setToken(newToken: string) {
-    token.value = newToken
-    localStorage.setItem('token', newToken)
+    token.value = newToken;
+    localStorage.setItem("token", newToken);
   }
 
-  function setUser(newUser: { id: string; name: string; email: string }) {
-    user.value = newUser
+  function setUser(newUser: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+  }) {
+    user.value = newUser;
+  }
+
+  function setMessage(err: { status: Number; messageTitle: string; message: string } | null) {
+    message.value = err;
   }
 
   function logout() {
-    token.value = null
-    user.value = null
-    localStorage.removeItem('token')
+    token.value = null;
+    user.value = null;
+    localStorage.removeItem("token");
   }
 
   return {
     token,
     user,
+    message,
     isAuthenticated,
     setToken,
     setUser,
+    setMessage,
     logout,
-  }
-})
+  };
+});
