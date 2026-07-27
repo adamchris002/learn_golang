05a6603b4f1e4c50c43351f1fd7d1c5fcbcd4045
Adam Christoper <adam2510025@gmail.com>
2026-07-27 20:21:30 +0700

    adjust page adjuster

--- interface/src/components/TaskLists.vue
+++ interface/src/components/TaskLists.vue
@@ -13,17 +13,19 @@ const emits = defineEmits<{
 <template>
     <div>
         <div class="w-200 mt-4 flex justify-start">
-            <p class="font-jakarta text-2xl text-white font-semibold">{{props.title}}</p>
+            <p class="font-jakarta text-2xl text-white font-semibold">{{ props.title }}</p>
         </div>
-        <div v-if="taskArray.length > 0 && taskArray" v-for="items in taskArray">
-            <div
-                class="mt-4 rounded-lg w-200 backdrop-blur-sm inset-shadow-[0_0_80px_rgba(0,0,0,0.25)] px-4 py-2 flex items-center">
-                <n-checkbox :checked="items.completed"
-                    @update-checked="(value: boolean) => emits('toggle', items.ID, value)" />
-                <div @click="emits('openTaskDetail', items)" class="w-full cursor-pointer">
-                    <p class="font-jakarta text-[#8a8888] text-base ml-4">Created at: {{
-                        dayjs(items.CreatedAt).format("DD/MMMM/YYYY HH:mm:ss") }}</p>
-                    <p class="font-jakarta text-white text-xl ml-4">{{ items.title }}</p>
+        <div v-if="taskArray.length > 0 && taskArray" class="max-h-[200px] overflow-y-auto custom-scroll">
+            <div v-for="items in taskArray">
+                <div
+                    class="mt-4 rounded-lg w-200 backdrop-blur-sm inset-shadow-[0_0_80px_rgba(0,0,0,0.25)] px-4 py-2 flex items-center">
+                    <n-checkbox :checked="items.completed"
+                        @update-checked="(value: boolean) => emits('toggle', items.ID, value)" />
+                    <div @click="emits('openTaskDetail', items)" class="w-full cursor-pointer">
+                        <p class="font-jakarta text-[#8a8888] text-base ml-4">Created at: {{
+                            dayjs(items.CreatedAt).format("DD/MMMM/YYYY HH:mm:ss") }}</p>
+                        <p class="font-jakarta text-white text-xl ml-4">{{ items.title }}</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,4 +34,21 @@ const emits = defineEmits<{
         </div>
     </div>
 </template>
-<style scoped></style>
+<style scoped>
+.custom-scroll::-webkit-scrollbar {
+    width: 6px;
+}
+
+.custom-scroll::-webkit-scrollbar-track {
+    background: transparent;
+}
+
+.custom-scroll::-webkit-scrollbar-thumb {
+    background: #4b5563;
+    border-radius: 9999px;
+}
+
+.custom-scroll::-webkit-scrollbar-thumb:hover {
+    background: #6b7280;
+}
+</style>
