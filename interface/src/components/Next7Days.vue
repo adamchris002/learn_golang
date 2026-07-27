406fb94e637dd58e75d7d0b83cdb8de481e7cae5
Adam Christoper <adam2510025@gmail.com>
2026-07-27 19:58:30 +0700

    add next 7 days feat

--- interface/src/components/Next7Days.vue
+++ interface/src/components/Next7Days.vue
@@ -1,7 +1,224 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import { NIcon, NInput, NButton, NCheckbox } from 'naive-ui';
+import dayjs from 'dayjs';
+
+import { useItemScale } from '@/composable/pageAdjuster';
+import { getOneWeekTasks, postTodaysTask, updateTaskCompletion, type TaskResponse } from '@/services/taskServices';
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+
+import send from "@/assets/icons/send.svg"
+import calendarIcon from '@/assets/icons/calendar.svg'
+
+dayjs.extend(customParseFormat)
+
+type dayTemplate = {
+    day: string,
+    taskDatas: TaskResponse[]
+}
+
+const authStore = useAuthStore()
+const scale = useItemScale()
+
+const user = JSON.parse(localStorage.getItem("user") || "{}")
+
+const taskArray = ref<TaskResponse[]>([])
+const taskInputArray = ref<string[]>([])
+
+const refreshTask = ref<boolean>(false)
+const days = ref<dayTemplate[]>([])
+const taskErrorMessage = ref<{ status: number; messageTitle: string; message: string } | null>(null)
+
+function showDays() {
+    days.value = []
+    let today = dayjs()
+
+    for (let i = 0; i < 7; i++) {
+        const day = today.add(i, "day")
+        const data = {
+            day: day.format("DD/MM/YYYY"),
+            taskDatas: taskArray.value.filter((data) => data.task_start === day.format("DD/MM/YYYY"))
+        }
+        days.value.push(data)
+    }
+}
+
+function todayOrTomorrowLabel(value: string) {
+    const dateValue = dayjs(value, "DD/MM/YYYY").format('dddd')
+    if (dateValue === dayjs().format('dddd')) {
+        return "Today"
+    } else if (dateValue === dayjs().add(1, 'day').format('dddd')) {
+        return "Tomorrow"
+    } else {
+        return
+    }
+}
+
+function initializeTaskInputArray() {
+    for (let i = 0; i < 7; i++) {
+        taskInputArray.value.push("")
+    }
+}
+
+async function addNewTask(index: number) {
+    const textString = taskInputArray.value[index]
+
+    const selectedDay = days.value[index]?.day
+
+    if (!selectedDay) {
+        return
+    }
+
+    if (textString && textString.length > 0) {
+        const newData = {
+            title: textString,
+            description: '',
+            task_start: selectedDay,
+            due_date: '',
+            completed: false,
+            userId: user.id
+        }
+        taskErrorMessage.value = await postTodaysTask(newData)
+        if (taskErrorMessage.value.status === 200) {
+            refreshTask.value = true
+        }
+    }
+    taskInputArray.value[index] = ""
+}
+
+async function updateTaskCheckbox(id: number, data: boolean) {
+    taskErrorMessage.value = await updateTaskCompletion(id, user.id, data)
+    if (taskErrorMessage.value.status === 200) {
+        refreshTask.value = true
+    }
+}
+
+async function callOneWeekTask() {
+    const result = await getOneWeekTasks(user.id);
+
+    if (result.success) {
+        taskArray.value = result.data.sort(
+            (a: TaskResponse, b: TaskResponse) =>
+                new Date(a.CreatedAt).getTime() -
+                new Date(b.CreatedAt).getTime()
+        );
+
+        refreshTask.value = false;
+    } else {
+        authStore.setMessage(result.messageData);
+    }
+}
+
+watch((refreshTask), async (newValue) => {
+    if (newValue) {
+        await callOneWeekTask()
+        showDays()
+    }
+}, { immediate: true })
+
+watch((days), (newValue) => { console.log(newValue) }, { immediate: true })
+
+onMounted(async () => {
+    await callOneWeekTask()
+    showDays()
+    initializeTaskInputArray()
+})
+</script>
 <template>
-    <div>
-        <h1 class="text-white">Next 7 Days</h1>
+    <div class="relative z-10 py-8 px-4 w-full h-screen overflow-x-auto custom-scroll">
+        <div
+            class="flex items-center backdrop-blur-sm inset-shadow-[0_0_80px_rgba(0,0,0,0.25)] size-fit rounded-xl py-2 px-4">
+            <n-icon size="16" class="mr-4">
+                <calendarIcon class="text-white " />
+            </n-icon>
+            <p class="font-jakarta text-white text-lg">Next 7 Days</p>
+        </div>
+        <div class="flex items-start w-full pt-8 scale-container"
+            :style="{ transform: `scale(${scale})`, transformOrigin: 'top left' }">
+            <div v-for="(value, index) in days" :key="index"
+                class="w-[300px] shrink-0 backdrop-blur-sm inset-shadow-[0_0_80px_rgba(0,0,0,0.25)] p-4 rounded-xl mr-8">
+                <div class="flex justify-start">
+                    <p
+                        :class="[typeof (todayOrTomorrowLabel(value.day)) === 'string' ? 'text-white mr-2 text-base' : '']">
+                        {{
+                            todayOrTomorrowLabel(value.day) }}</p>
+                    <p
+                        :class="[typeof (todayOrTomorrowLabel(value.day)) === 'string' ? 'text-[#9c9c9c] text-base' : 'text-white text-base']">
+                        {{
+                            dayjs(value.day, "DD/MM/YYYY").format('dddd') }}</p>
+                </div>
+                <div class="my-4 overflow-y-auto max-h-[600px] custom-scroll">
+                    <div v-if="value.taskDatas.length > 0" v-for="(items, childIndex) in value.taskDatas"
+                        :key="childIndex" :class="[childIndex > 0 ? 'mt-2' : '']">
+                        <div class="flex justify-start items-center cursor-pointer">
+                            <n-checkbox :checked="items.completed"
+                                @update-checked="(value: boolean) => updateTaskCheckbox(items.ID, value)" />
+                            <div class="ml-2 p-2 bg-[#262626] rounded-md">
+                                <p class="font-jakarta text-[#8a8888] text-sm">Created At: {{
+                                    dayjs(items.CreatedAt).format("DD/MM/YYYY HH:mm:ss") }}</p>
+                                <p class="text-white font-jakarta text-base">{{ items.title }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <n-input v-model:value="taskInputArray[index]" block type="textarea" :resizable="false"
+                    placeholder="+ Add Task"
+                    :theme-overrides="{ color: 'backdrop-blur-sm', borderHover: '1px solid #0373fc', borderFocus: '1px solid #0373fc', colorFocus: 'backdrop-blur-sm', textColor: 'white' }">
+                    <template #suffix>
+                        <n-button :bordered="false" @click="addNewTask(index)"
+                            :theme-overrides="{ borderHover: '1px solid #0373fc', borderFocus: '1px solid #0373fc', rippleColor: 'none' }"
+                            class="send-btn">
+                            <n-icon>
+                                <send />
+                            </n-icon>
+                        </n-button>
+                    </template>
+                </n-input>
+            </div>
+            <!-- additional div for space at end -->
+        </div>
     </div>
 </template>
-<style scoped></style>
+<style scoped>
+.scale-container {
+    transition: transform 300ms ease-in-out;
+}
+
+.send-btn {
+    color: white;
+}
+
+.send-btn:hover {
+    color: #0373fc;
+}
+
+.send-btn:focus {
+    color: #0373fc;
+}
+
+:deep(.n-input__suffix) {
+    margin-right: -12px !important;
+}
+
+:deep(.n-input__textarea.n-scrollbar) {
+    height: 34px !important
+}
+
+.custom-scroll::-webkit-scrollbar {
+    width: 6px;
+}
+
+.custom-scroll::-webkit-scrollbar-track {
+    background: transparent;
+}
+
+.custom-scroll::-webkit-scrollbar-thumb {
+    background: #4b5563;
+    border-radius: 9999px;
+}
+
+.custom-scroll::-webkit-scrollbar-thumb:hover {
+    background: #6b7280;
+}
+</style>
